99206a283712500c0c4263f72899bd82a51b1436
medlaq777 <m.laqsoumi@gmail.com>
2025-03-12 14:10:26 +0000

    make the initial functionality for CarController and test it with postman

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Car;
 
 class CarController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(Car::all());
+    }
+
+    public function show($id)
+    {
+        return response()->json(Car::find($id));
+    }
+
+
+    public function delete(Request $request, $id)
+    {
+        $car = Car::findOrFail($id);
+        $car->delete();
+        return response()->json(null, 204);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,27 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CarController;
 
+
+
+
+// AUTHENTICATION
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
     Route::get('profile', [AuthController::class, 'profile']);
 });
+
+// CARS
+
+Route::get('cars', [CarController::class, 'index']);
+Route::get('cars/{id}', [CarController::class, 'show']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('cars', [CarController::class, 'store']);
+    Route::put('cars/{id}', [CarController::class, 'update']);
+    Route::delete('cars/{id}', [CarController::class, 'delete']);
+});
+
 
