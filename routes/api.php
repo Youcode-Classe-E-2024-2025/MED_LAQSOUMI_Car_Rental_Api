374beeeac586e1e206b578b2db443654050437be
medlaq777 <m.laqsoumi@gmail.com>
2025-03-12 11:09:18 +0000

    complet the Authentication part

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-// Route::apiResource('users', [AuthController::class]); 
-
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('profile', [AuthController::class, 'profile']);
 });
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
