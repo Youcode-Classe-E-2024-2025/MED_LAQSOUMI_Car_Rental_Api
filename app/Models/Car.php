a5b75319868026b88ad32f467b019825b11151de
medlaq777 <m.laqsoumi@gmail.com>
2025-03-14 21:42:11 +0000

    fix models rentals issues

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 
 class Car extends Model
 {
     
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
     protected $fillable = [
         'name',
         'brand',
