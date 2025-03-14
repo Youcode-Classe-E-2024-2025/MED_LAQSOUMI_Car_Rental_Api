e36747443ea6015a8f0b36c5b4960ecc2d8d868c
medlaq777 <m.laqsoumi@gmail.com>
2025-03-14 21:49:59 +0000

    make factory and seeders for rentals data

--- database/factories/RentalsFactory.php
+++ database/factories/RentalsFactory.php
@@ -22,7 +22,7 @@ class RentalsFactory extends Factory
             'car_id' => $this->faker->numberBetween(1, 10),
             'from' => $this->faker->dateTimeBetween('-1 month', 'now')->format('Y-m-d'),
             'to' => $this->faker->dateTimeBetween('now', '+1 month')->format('Y-m-d'),
-            'total_price' => $this->faker->randomFloat(2, 100, 1000),
+            'total_price' => $this->faker->randomNumberBetween(100, 1000),
             'created_at' => $this->faker->date(),
             'updated_at' => $this->faker->date(),
         ];
