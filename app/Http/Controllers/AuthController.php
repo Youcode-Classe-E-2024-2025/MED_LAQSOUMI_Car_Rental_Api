9df9511f999b0361d5faed35d8e5da766cbbb549
medlaq777 <m.laqsoumi@gmail.com>
2025-03-14 17:39:43 +0000

    fix auth issues and now is work better

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,30 +7,37 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class AuthController extends Controller
 {
 	
 	public function register(Request $request)
-	{
-		$validator = Validator::make($request->all(), [
-			'name' => 'required|string|max:255',
-			'email' => 'required|string|email|max:255|unique:users',
-			'password' => 'required|string|min:8|confirmed',
-		]);
+{
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:8|confirmed',
+    ]);
 
-		$user = User::create([
-			'name' => $request->name,
-			'email' => $request->email,
-			'password' => Hash::make($request->password),
-		]);
+    if ($validator->fails()) {
+        return response()->json($validator->errors(), 422);
+    }
 
-		return response()->json([
-			'message' => 'User registered successfully',
-			'user' => $user,
-			'token' => $this->createToken($user)
-		], 201);
-	}
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'email_verified_at' => now(),
+        'password' => Hash::make($request->password),
+        'remember_token' => Str::random(10),
+    ]);
+
+    return response()->json([
+        'message' => 'User registered successfully',
+        'user' => $user,
+        'token' => $this->createToken($user)
+    ], 201);
+}
 
 	public function login(Request $request)
 	{
